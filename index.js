e91fa4894d6b38d92919b547321c2914206fa767
rickylolo <ricky_lolo29@hotmail.com>
2023-07-13 17:06:18 -0600

    v1.0.1-Alpha1

--- index.js
+++ index.js
@@ -1,34 +1,61 @@
-// Mis modulos de terceros
 const express = require('express')
 const router = express.Router()
+const pdfjsLib = require('pdfjs-dist/build/pdf')
 
 // Creo mi aplicación express
 const app = express()
 
-//Especifico el puerto a utilizar
+// Especifico el puerto a utilizar
 const port = process.env.PORT || 3000
 
 // Especifico que express use JSON para el body
 app.use(express.json({ limit: '50mb' }))
 
-//Declaro un objeto que contenga mis funciones para mi PDF
+// Declaro un objeto que contenga mis funciones para mi PDF
 const PDF = {
   getTextFromPDF: async (req, res) => {
     try {
-      res.send(req.body)
-    } catch (err) {
-      res.status(500).send(err.message)
+      const { pdf, searchWords } = req.body
+
+      const buffer = Buffer.from(pdf, 'base64')
+      const data = new Uint8Array(buffer)
+      const doc = await pdfjsLib.getDocument(data).promise
+      const numPages = await doc.numPages
+      let text = ''
+
+      for (let i = 1; i <= numPages; i++) {
+        const page = await doc.getPage(i)
+        const content = await page.getTextContent()
+        const pageText = content.items.map((item) => item.str).join(' ')
+        text += pageText
+      }
+
+      // Buscar palabras en el texto obtenido del PDF
+      const foundWords = {}
+      searchWords.forEach((word) => {
+        const regex = new RegExp(`\\b${word}\\b(?=:)(.*?)(?=\\b\\w+:|$)`, 'g')
+        const matches = text.match(regex)
+        if (matches) {
+          foundWords[word] = matches.map((match) => match.trim())
+        } else {
+          foundWords[word] = []
+        }
+      })
+
+      res.status(200).json({ text, foundWords })
+    } catch (error) {
+      res.status(500).send(`Error al extraer el texto: ${error}`)
     }
   },
 }
 
-//Le asigno al router las acciones y su función a realizar
+// Le asigno al router las acciones y su función a realizar
 router.post('/PDF', PDF.getTextFromPDF)
 
 // Agrego mis rutas para crear los endpoints de las entidades
 app.use('/process', router)
 
-// Cualquier otra ruta que no este definida arroja un status 404 page not found
+// Cualquier otra ruta que no esté definida arroja un status 404 page not found
 app.get('*', (req, res) => {
   res.status(404).send('Esta página no existe')
 })
